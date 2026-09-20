c7a82a9b68264aff17a7b22bbbdfc446b376bdfd
Keertika Devwanshi <kirtikaakanksha@gmail.com>
2026-09-20 06:20:14 +0000

    var_js

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountid = "KEERTIKA DEVWANSHI" ;
+let email = "keertika@gmail.com";
+currentcity = "greater noida";
+ email ="devwanashi@Gmail.com";
+ currentcity ="varanasi";
+
+ console.table([accountid,email,currentcity]);
+ 
